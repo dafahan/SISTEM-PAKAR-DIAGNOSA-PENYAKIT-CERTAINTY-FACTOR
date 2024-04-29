62e00b475656ff746759adfde2b73be397a74a26
farkmu45 <farukmicros@outlook.com>
2024-04-30 07:39:57 +0800

    Add authentication for user

---
+++ app/Filament/Pages/Auth/Register.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Pages\Auth;
+
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Pages\Auth\Register as BaseRegister;
+
+class Register extends BaseRegister
+{
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                $this->getNameFormComponent(),
+                $this->getEmailFormComponent(),
+                Radio::make('gender')
+                    ->required()
+                    ->options([
+                        'MALE' => 'Male',
+                        'FEMALE' => 'Female',
+                    ])
+                    ->inline(),
+                TextInput::make('age')
+                    ->maxValue(200)
+                    ->numeric()
+                    ->minValue(1)
+                    ->required(),
+                $this->getPasswordFormComponent(),
+                $this->getPasswordConfirmationFormComponent(),
+            ]);
+    }
+}
