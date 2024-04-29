baec28395c9f91b9f7ec624f3cb39d7d678ba726
farkmu45 <farukmicros@outlook.com>
2024-04-29 21:19:08 +0800

    Restructure user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->unsignedMediumInteger('age');
+            $table->enum('gender', ['MALE', 'FEMALE']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
